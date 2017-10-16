473fdadd8443043d4993c501313a95e9c12f497b
quynh <quynhnguyenceo97@gmail.com>
2017-10-16 09:20:08 +0700

    đã thêm và sửa code

--- index.php
+++ index.php
@@ -2,18 +2,40 @@
 /**
  * summary
  */
-class Calculato
+class Calculato 
 {
     /**
      * summary
      */
-    public function __construct()
-    {
-        
-    }
-    public function hello($world)
-    {
-    	echo 'Hello '.$world;
-    }
+	 
+	public function __construct()
+	{
+		   
+	}
+	
+	public function hello($world)
+	{
+		echo 'Hello '.$world;
+	}
+	public function tinhtong($a,$b)
+	{
+		$tong=$a+$b;
+		return $tong;
+	}
+	public function tinhhieu($a,$b)
+	{
+		$hieu=$a-$b;
+		return $hieu;
+	}
+	public function tinhtich($a,$b)
+	{
+		$tich=$a*$b;
+		return $tich;
+	}
+	public function tinhthuong($a,$b)
+	{
+		$thuong=$a/$b;
+		return $thuong;
+	}
 }
 ?>
