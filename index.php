8d51227ea9a7e35844ed4278513f8e7464b02ea6
Quyet Tran <zangquyet@gmail.com>
2017-10-16 08:46:58 +0700

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * summary
+ */
+class Calculato
+{
+    /**
+     * summary
+     */
+    public function __construct()
+    {
+        
+    }
+    public function hello($world)
+    {
+    	echo 'Hello '.$world;
+    }
+}
+?>
